f16def37a24c68a67eb5837e2a6d1083c879fe13
hkinoshita54 <kinosita.hiroto@gmail.com>
2023-11-06 17:54:56 +0900

    First commit

---
+++ notebook/00_test.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# ! ls
+
+# %% language="bash"
+# ls
+
+# %%
+import pandas as pd
+
+# %%
+pd.Series([1, 1, 2, 3, 5, 8, 13])
+
+# %%
